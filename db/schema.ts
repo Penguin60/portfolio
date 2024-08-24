7756867e693bcd46fe57b295d8627ecff4856fc2
Dean <radean.rashed@gmail.com>
2024-08-23 20:57:21 -0400

    Initialize database schema

--- db/schema.ts
+++ db/schema.ts
@@ -1,27 +1,24 @@
+import { sql } from 'drizzle-orm';
 import { integer, pgTable, serial, text, timestamp } from 'drizzle-orm/pg-core';
 
 export const projectsTable = pgTable('projects_table', {
   id: serial('id').primaryKey(),
-  name: text('name').notNull(),
-  age: integer('age').notNull(),
-  email: text('email').notNull().unique(),
+  title: text('title').notNull(),
+  link: text('link').notNull(),
+  image: text('image').notNull(),
+  tags: text('tags').array().notNull().default(sql`'{}'::text[]`),
+  description: text('description').notNull(),
 });
 
-export const postsTable = pgTable('posts_table', {
+export const blogsTable = pgTable('blog_table', {
   id: serial('id').primaryKey(),
   title: text('title').notNull(),
   content: text('content').notNull(),
-  userId: integer('user_id')
-    .notNull()
-    .references(() => projectsTable.id, { onDelete: 'cascade' }),
   createdAt: timestamp('created_at').notNull().defaultNow(),
-  updatedAt: timestamp('updated_at')
-    .notNull()
-    .$onUpdate(() => new Date()),
 });
 
-export type InsertUser = typeof projectsTable.$inferInsert;
-export type SelectUser = typeof projectsTable.$inferSelect;
+export type InsertProject = typeof projectsTable.$inferInsert;
+export type SelectProject = typeof projectsTable.$inferSelect;
 
-export type InsertPost = typeof postsTable.$inferInsert;
-export type SelectPost = typeof postsTable.$inferSelect;
+export type InsertBlog = typeof blogsTable.$inferInsert;
+export type SelectBlog = typeof blogsTable.$inferSelect;
