357bb51289adcc66b4aa0687ab7f05d4bb2897bd
Dean <radean.rashed@gmail.com>
2025-01-01 20:59:04 -0500

    Add basic home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,51 @@
-export default function Home() {
+import { Card } from "@/components/ui/card";
+import { Avatar, AvatarImage, AvatarFallback } from "@/components/ui/avatar";
+import { getProjects } from "@/server/queries";
+
+export default async function Home() {
+  const projects = await getProjects();
+  const projectsCount = projects.length;
+
+  function formatCount(count: number) {
+    return count < 10 ? `0${count}` : count.toString();
+  }
+
   return (
     <main className="bg-white dark:bg-zinc-950 text-black dark:text-white items-center flex justify-center h-full">
-      <h1 className="text-black dark:text-white text-9xl">Soon...</h1>
+      <Card className="w-[97vw] h-[88vh] flex">
+        <div className="flex flex-col w-full h-full">
+          <div className="m-8 flex items-center">
+            <Avatar className="w-14 h-14">
+              <AvatarImage src="/profile.png" />
+              <AvatarFallback>RR</AvatarFallback>
+            </Avatar>
+            <p className="ml-3 text-lg">
+              <strong>Hey there</strong>, I'm <strong>Radean</strong>
+            </p>
+          </div>
+          <p className="m-8 mt-0 text-lg">
+            I’m a 10th grade IB student with a passion for{" "}
+            <strong>programming</strong>, <strong>robotics</strong> and{" "}
+            <strong>mathematics</strong>.
+          </p>
+          <p className="m-8 mt-0 text-lg">
+            I love learning new skills, experimenting with technology and
+            creating fun projects with my{" "}
+            <a href="https://github.com/orgs/STRNerds/" className="underline">
+              friends
+            </a>
+            !
+          </p>
+          <p className="m-8 mt-0 text-lg">
+            When I’m not coding, you’ll find me <strong>reading</strong>,
+            playing <strong>tennis</strong> or cramming for exams.
+          </p>
+          <div className="m-8 mt-auto">
+            <h1 className="text-5xl font-bold">{formatCount(projectsCount)}</h1>
+            <p className="mt-1">Projects created</p>
+          </div>
+        </div>
+      </Card>
     </main>
   );
 }
